345cf071ca03977f9fe91ca501cadd614d91b3fa
N8rown <146596632+N8rown@users.noreply.github.com>
2026-04-04 15:38:36 -0700

    Fixed Block, Started Chunk

--- NinecraftByNojang/src/ninecraftbynojang/Block.java
+++ NinecraftByNojang/src/ninecraftbynojang/Block.java
@@ -14,12 +14,13 @@ public class Block {
     private float x,y,z;
     
     public enum BlockType {
+        BlockType_Default(-1),
         BlockType_Grass(0),
         BlockType_Dirt(1),
         BlockType_Water(2),
         BlockType_Sand(3),
         BlockType_Stone(4),
-        BlockType_Bedrock5);
+        BlockType_Bedrock(5);
         private int BlockID;
         BlockType(int i){
             BlockID=i;
@@ -31,5 +32,28 @@ public class Block {
         public void SetID(int i){
             BlockID = i;
         }
+    }
+    
+    public Block(BlockType type)
+    {
+        Type = type;
+    }
+    public void setCoords(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public boolean isActive()
+    {
+        return isActive;
+    }
+    public void setActive(boolean setTo)
+    {
+        isActive = setTo;
+    }
+    public int GetID()
+    {
+        return Type.GetID();
     }
 }
